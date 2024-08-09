5c7b4b0acf64de5ef8be8d059a24bcf01fd21bd7
Risto Kuusela <risto.kuusela@unikie.com>
2024-08-09 16:03:33 +0300

    Wireguard-gui: set GTK debug messages on
    Signed-off-by: Risto Kuusela <risto.kuusela@unikie.com>

--- packages/wireguard-gui/default.nix
+++ packages/wireguard-gui/default.nix
@@ -6,8 +6,11 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "remimimimimi";
     repo = pname;
+    # rev = "5efd2c41b466616444d4acb341887e1197c8e801";
     rev = "ab5083daf9fb8fb4c0e349882d2554aca1286ec2";
+    #sha256 = "sha256-lZqPCFcbkdZmoowYX0yG37oe+2HuaYmCTwsl84LHRV8=";
     sha256 = "sha256-+E7mnhe6uaJVgETWEfIrCfAhSRamPlMcxG6P3BDGneE=";
+    # sha256 = lib.fakeSha256;
   };
 
   nativeBuildInputs = [
@@ -23,7 +26,8 @@ rustPlatform.buildRustPackage rec {
 
   postFixup = ''
     wrapProgram $out/bin/${pname} \
-       --set LIBGL_ALWAYS_SOFTWARE true
+       --set LIBGL_ALWAYS_SOFTWARE true \
+       --set G_MESSAGES_DEBUG all
   '';
 
   cargoSha256 = "sha256-rV+GAOd3BmbMZKDKRDFNzrSbi5IqptNoFo9wHRDBPT0=";
